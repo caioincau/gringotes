c9c0934f9a794264ada1c35e50f32580d82b7552
Caio Incau <caioincau@gmail.com>
2018-02-20 08:32:33 -0300

    CoinList

---
+++ src/components/CoinTable/CoinTable.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import CoinTable from './CoinTable';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+let coins = [{
+  id: '1',
+  rank: '1',
+  symbol: 'EC',
+  name: 'ECoin',
+  price_usd: '23232',
+  '24h_volume_usd': '312321321321'
+},
+{
+  id: '2',
+  rank: '1',
+  symbol: 'BTC',
+  name: 'Bitcoin',
+  price_usd: '23232',
+  '24h_volume_usd': '312321321321'
+}]
+
+let wrapper = {}
+
+
+let mountComponent = (filter= '') => {
+  return Enzyme.mount(
+    <CoinTable coins={coins} filter={filter}></CoinTable>
+  );
+}
+beforeEach(() => {
+  wrapper = mountComponent()
+});
+
+it('renders without crashing', () => {
+  expect(wrapper.find('.coin-item').length).toEqual(2);
+});
+
+it('should filter', () => {
+  wrapper = mountComponent('EC')
+  expect(wrapper.find('.coin-item').length).toEqual(1);
+});
+
+
